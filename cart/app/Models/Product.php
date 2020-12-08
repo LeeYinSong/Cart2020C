ac68f522afc4d0b85bd61fce84793c475f6772bb
LeeYinSong <Yinsonglee00@gmail.com>
2020-12-09 01:12:34 +0800

    ...

--- cart/app/Models/Product.php
+++ cart/app/Models/Product.php
@@ -9,7 +9,7 @@ class Product extends Model
 {
     use HasFactory;
 
-    protected $fillable=['name','description','price','imaage','quantity','categoryID'];
+    protected $fillable=['name','description','price','image','quantity','categoryID'];
     public function category(){
         return $this->belongsTo('App\Models\Category');
     }
